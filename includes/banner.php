3475841ad442c95823ceb5bf6a0f4ecfebd293a7
Rafael Damasio <damasio_damasio@hotmail.com>
2014-03-18 04:41:55 -0300

    banners

--- includes/banner.php
+++ includes/banner.php
@@ -1,18 +1,27 @@
 <?php
 	include("includes/config.php");
-	$sql 	= "SELECT `id`, `arquivo` FROM `imagens` WHERE `destaque` = 1 AND `ativo` = 1 ORDER BY RAND() LIMIT 3";
-	$query 	= mysql_query($sql);
+	$sql 			= "SELECT `id`, `arquivo` FROM `imagens` WHERE `destaque` = 1 AND `ativo` = 1 ORDER BY RAND() LIMIT 3";
+	$query 			= mysql_query($sql);
+	$num_rows_img	= mysql_num_rows($query);
 ?>
 
 <div class="banner" >
 	<div class="wrapper-thumb">
 		<ul>
 			<?php
-				while($dados = mysql_fetch_array($query)){
+				if($num_rows_img > 0){
+					while($dados = mysql_fetch_array($query)){
 			?>
-					<li>
-						<img src="administrator/uploads/images/<?php echo $dados["arquivo"]; ?>" alt="" width="168" height="94" rel="<?php echo $dados["id"] ?>">
-					</li>
+						<li>
+							<img src="administrator/uploads/images/<?php echo $dados["arquivo"]; ?>" alt="" width="168" height="94" rel="<?php echo $dados["id"] ?>">
+						</li>
+			<?php
+					}
+				} else {
+			?>
+						<li>
+							<p style="padding: 1em;">Nenhuma imagem encontrada!</p>
+						</li>
 			<?php
 				}
 			?>
@@ -48,45 +57,3 @@
 		});
 	});
 </script>
-
-
-
-
-
-
-<!-- <div class="banner" >
-	<div class="wrapper-thumb">
-		<span id="#controlTop"></span>
-		<ul>
-			<li>
-				<a href="#foto1">
-					<img src="images/banner2.jpg" alt="">
-				</a>
-			</li>
-			<li>
-				<a href="#foto2">
-					<img src="images/banner3.jpg" alt="">
-				</a>
-			</li>
-			<li> 
-				<a href="#foto3">
-					<img src="images/banner4.jpg" alt="">
-				</a>
-			</li>
-		</ul>
-		<span id="#controlBottom"></span>
-	</div>
-	<div class="wrapper-banner">
-		<ul>
-			<li id="foto1" class="">
-				<img src="images/banner1.jpg" alt="" >
-			</li>
-			li id="foto2">
-				<img src="images/slide2.png" width="700" alt="" class="">
-			</li>
-			<li id="foto3">
-				<img src="images/slide3.png"  width="700" alt="" class="">
-			</li> 
-		</ul>
-	</div>
-</div> -->
